d342638e3367f87c3de1bf4114893037dbf13ee8
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2020-04-05 01:56:17 +0900

    Update My Server : add 'Create' function

--- NodeJS/kmc_web_server/main.js
+++ NodeJS/kmc_web_server/main.js
@@ -1,6 +1,7 @@
 var http = require('http'); // http protocol
 var fs = require('fs'); // filesystem
 var url = require('url'); // use module 'url'
+var qs = require('querystring');
 
 function makeHTML(title, data, list){
   var my_list = list;
@@ -20,6 +21,7 @@ function makeHTML(title, data, list){
     <ol>
       ${my_list}
     </ol>
+    <a href="/create">CREATE</a>
     <h2>${title}</h2>
     <p>${data}</p>
   </body>
@@ -56,33 +58,76 @@ var app = http.createServer(function(request,response){
 
     // Get Query.
     var queryData = url.parse(req_url, true).query; // extract query , url = "localhost:3000/?id=HTML"
+    var queryPathname = url.parse(req_url, true).pathname;
     var query_id = queryData.id;
 
-    if(queryData.id === undefined){
-      if(req_url == '/') {// home
+    if(queryPathname === '/') { // home
+      if(queryData.id === undefined) {
         req_url = '/?id=home';
         query_id = 'home';
       }
-      else{ // nothing.
-        var undefined_page = makeHTML('Fail to Found', 'Please check your URL');
-        response.writeHead(404); // ERROR MESSAGE HEADER
-        response.end(undefined_page);
-        return response;
-      }
-    }
 
-    // Make Response Message.
-    fs.readdir('./data', function(err, dir_list){
+      // Make Response Message.
+      var dir_list = fs.readdirSync('./data', 'utf8');
       var result_list = makeList_ul(dir_list, dir_list.length);
 
-      fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, file_data){
-        var result_html = makeHTML(query_id, file_data, result_list);
+      var file_data = fs.readFileSync(`./data/${query_id}`, 'utf8');
+      var result_html = makeHTML(query_id, file_data, result_list);
 
-        response.writeHead(200); // ERROR MESSAGE HEADER
-        response.end(result_html);
+      response.writeHead(200); // ERROR MESSAGE HEADER
+      response.end(result_html);
+
+      return response;
+    } // end of if home
+    else if(queryPathname === "/create") { // create
+      var create_title = 'This Is Create Menu'
+      var create_data = `
+      <form action="http://localhost:8080/create_data" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+
+        <p><textarea name="description" rows="10" cols="40" placeholder="description"></textarea></p>
+
+        <p><input type="submit"></p>
+      </form>
+      `;
+
+      var result_html = makeHTML(create_title, create_data, "");
+
+      response.writeHead(200);
+      response.end(result_html);
+    } // end of if create
+    else if(queryPathname === '/create_data') { // get POST Message.
+      // parameter of this web server, 'Request'
+
+      var create_body = '';
+
+      request.on('data', function(data){ // when receive request. Do this.
+        create_body = create_body + data;
       });
-    }); // end of Making Response Message.
+
+      request.on('end', function(){ // no remain data => end, Do this.
+        var post = qs.parse(create_body);
+        // post.tile => create title.
+        // post.description => create description.
+
+        fs.writeFile(`data/${post.title}`, post.description, 'utf8', function(err) {
+          console.log('Creating File is Successed!');
+        });
+
+        response.writeHead(302, {Location: `/?id=${post.title}`}); // 302 => Redirection
+        response.end();
+      });
+
+
+    } // end of get POST
+    else { // not found
+      var undefined_page = makeHTML('Fail to Found', 'Please check your URL');
+      response.writeHead(404);
+      response.end(undefined_page);
+    } // end of not found
+
+    return response;
 
 }); // end of Server
 
-app.listen(3000); // port number 3000.
+app.listen(8080); // port number 3000.
