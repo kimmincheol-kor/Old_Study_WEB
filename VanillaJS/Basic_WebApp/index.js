7391e20a1ac48536d68765a29e1c9a9eaf8307ce
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-08-13 23:28:19 +0900

    Tutorial : Modifying DOM

--- VanillaJS/Basic_WebApp/index.js
+++ VanillaJS/Basic_WebApp/index.js
@@ -1,1 +1,25 @@
-alert('hi vanilla');
+// alert('hi vanilla');
+
+const my_title = document.getElementById("my_title");
+
+function sayHello(username) {
+    alert(`Hello ${username}`);
+}
+
+sayHello(my_title.innerText);
+
+// DOM : Document Object Model
+// Modifying DOM !!
+
+console.dir(my_title);
+
+my_title.innerText = "Second";
+my_title.style.color = "blue";
+
+console.dir(document);
+
+document.title = "Tutorial-2-!"
+
+const finding = document.querySelector("#my_title");
+
+console.log(finding);
